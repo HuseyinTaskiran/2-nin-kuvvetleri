a995f00e28b43ebc4d6edd5aea4d26b96e62ff32
Hüseyin Taşkıran <huseyintaskiran1994@gmail.com>
2023-01-17 20:18:07 +0300

    Hüseyin Taşkıran
    kodlar aktarildi

---
+++ javapatika/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int i=2,n;
+        System.out.print("bir sayı giriniz : ");
+        n=inp.nextInt();
+        while (i<=n){
+            System.out.println(i);
+                i*=2;
+
+            }
+
+        }
+
+
+
+
+
+
+
+    }
